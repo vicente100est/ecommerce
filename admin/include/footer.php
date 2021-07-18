06219f4513f48b5cfdc6a56c7d7b058e43c0e8eb
Vicente Estrada <vicenteestradomin@gmail.com>
2021-07-17 20:48:48 -0500

    Aportación de vicente a la comunidad

---
+++ admin/include/footer.php
@@ -0,0 +1,7 @@
+	<div class="footer">
+		<div class="container">
+			 
+
+			<b class="copyright">&copy; 2020 Tienda Virtual - <a href="http://behappywoman.store/"> Be Happy Woman</a> </b> Todos los derechos reservados.
+		</div>
+	</div>
